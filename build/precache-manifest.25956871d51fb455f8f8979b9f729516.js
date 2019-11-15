5c4e88ecae827a2483b2017f055775ae4c8418a0
kuchad-git <57764957+kuchad-git@users.noreply.github.com>
2019-11-15 23:02:31 +0500

    Add files via upload

---
+++ build/precache-manifest.25956871d51fb455f8f8979b9f729516.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996c5b30a49ae79e550b120fbf170a9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f1235d0e6b661b310792",
+    "url": "./static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "5577f4a73a7d959f7aba",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "f1235d0e6b661b310792",
+    "url": "./static/js/2.5b41e883.chunk.js"
+  },
+  {
+    "revision": "5577f4a73a7d959f7aba",
+    "url": "./static/js/main.6a48249e.chunk.js"
+  },
+  {
+    "revision": "879762446eed2a92b761",
+    "url": "./static/js/runtime-main.1bdc3ff1.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
